835f5657071fff57e779e77c2af730c8df57d4d3
Hargoze <francois.lecarpentier99@gmail.com>
2020-09-16 14:29:55 +0200

    migrate to mongodb atlas

--- config/database.js
+++ config/database.js
@@ -1,15 +1,17 @@
+require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` })
+
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-      },
-      options: {
-        useNullAsDefault: true,
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: 'mongoose',
+        settings: {
+          client: 'mongo',
+          uri: `mongodb://admin:${process.env.MONGO_PWD}@cluster0-shard-00-00.u3axo.mongodb.net:27017,cluster0-shard-00-01.u3axo.mongodb.net:27017,cluster0-shard-00-02.u3axo.mongodb.net:27017/test?ssl=true&replicaSet=atlas-g14h2b-shard-0&authSource=admin&retryWrites=true&w=majority`
+        },
+        options: {
+          "ssl": true
+        },
       },
     },
-  },
-});
+  });
